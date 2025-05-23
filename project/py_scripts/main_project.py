619eff16d2048e29e25688a854af3aa5280cfd2e
TheWebsiteMan <xavier.pritchett@education.nsw.gov.au>
2025-05-23 12:52:52 +1000

    complete digital sensor

---
+++ project/py_scripts/main_project.py
@@ -0,0 +1,27 @@
+from time import sleep
+from machine import Pin
+
+
+sleep(0.1)
+
+led_pin = 25
+led2_pin = 15
+data_pin = 13
+analog_data_pin = 26
+
+led = Pin(led_pin, Pin.OUT)
+led2 = Pin(led2_pin, Pin.OUT)
+
+data = Pin(data_pin, Pin.IN)
+
+adc_value = ADC(Pin(analog_data_pin))
+
+while True:
+    if data.value() == 1:
+        led.value(True)
+        led2.value(False)
+    else:
+        led.value(False)
+        led2.value(True)
+    print(print(f"Analog: {adc_value}"))
+    sleep(0.1)
