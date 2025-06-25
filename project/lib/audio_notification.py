790b41c1653b80ea140e3fc356b82af2e02d78f5
TheWebsiteMan <xavier.pritchett@education.nsw.gov.au>
2025-06-25 15:04:03 +1000

    added audio notification

--- project/lib/audio_notification.py
+++ project/lib/audio_notification.py
@@ -1,8 +1,29 @@
-class Audio_Notification:
-    def __init__(self, pin, debug):
-        self.__pin = pin
-        self.__debug = False
+from machine import Pin, PWM
+from time import sleep, time
+
+
+class Audio_Notification(PWM):
+    def __init__(self, pin, debug=False):
+        super().__init__(Pin(pin))
+        self.__debug = debug
+        self.duty_u16(0)
+        self._last_toggle_time = time()
+
     def warning_on(self):
-        print("Beep")
+        if self.__debug:
+            print("Warning on")
+        now = time()
+        if now - self._last_toggle_time >= 0.5:
+            self.beep(freq=500, duration=100)
+            self._last_toggle_time = now
+
     def warning_off(self):
-        pass
+        if self.__debug:
+            print("Warning off")
+        self.duty_u16(0)
+
+    def beep(self, freq=1000, duration=500):
+        self.freq(freq)
+        self.duty_u16(32768)  
+        sleep(duration / 1000)
+        self.duty_u16(0)
